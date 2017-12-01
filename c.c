ecac659254a7d89723a92fd0940ca543dcbae7b5
Ashishx88 <alanbombe@gmail.com>
2017-12-01 12:06:46 +0530

    Adds GPL License
    Adds message of GPL license

--- c.c
+++ c.c
@@ -1,3 +1,17 @@
+/*
+ * This is an sample code to print Hello World on console
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #include <stdio.h>
 
 int main() {
